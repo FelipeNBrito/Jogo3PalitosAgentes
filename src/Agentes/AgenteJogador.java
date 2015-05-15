3617763f51275e79c06dcc53250643586e532f16
FelipeNBrito <felipenevesbrito@gmail.com>
2015-05-15 18:44:15 -0300

    Criação do repositorio

---
+++ src/Agentes/AgenteJogador.java
@@ -0,0 +1,10 @@
+package Agentes;
+
+import jade.core.Agent;
+
+public class AgenteJogador extends Agent {
+
+	protected void setup(){
+		
+	}
+}
---
+++ src/Agentes/AgenteMediador.java
@@ -0,0 +1,10 @@
+package Agentes;
+
+import jade.core.Agent;
+
+public class AgenteMediador extends Agent{
+	
+	protected void setup(){
+		
+	}
+}
